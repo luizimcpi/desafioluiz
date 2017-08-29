d5817319659cbde9ffdb641f616614625f2eae20
Luiz <luizhenrique.se@gmail.com>
2017-08-28 21:43:56 -0300

    Correct Charset

--- src/main/java/com/concrete/desafioluiz/keys/ErrorKeys.java
+++ src/main/java/com/concrete/desafioluiz/keys/ErrorKeys.java
@@ -1,11 +1,11 @@
 package com.concrete.desafioluiz.keys;
 
 public enum ErrorKeys {
-	EMAIL_EXISTENTE("E-mail já existente"),
-	UNAUTHORIZED("Não autorizado"),
-	PASSWORD_ENCRYPT_ERROR("Ocorreu um erro ao criptografar a senha do usuário, tente cadastrar novamente"),
-	INAVLID_CREDENTIALS("Usuário e/ou senha inválidos"),
-	INVALID_SESSION("Sessão inválida");
+	EMAIL_EXISTENTE("E-mail ja existente"),
+	UNAUTHORIZED("Nao autorizado"),
+	PASSWORD_ENCRYPT_ERROR("Ocorreu um erro ao criptografar a senha do usuario, tente cadastrar novamente"),
+	INAVLID_CREDENTIALS("Usuario e/ou senha invalidos"),
+	INVALID_SESSION("Sessao invalida");
 	
 	private final String name;       
 
