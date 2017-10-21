8549cd100bd3d1f2b2dbdd48e34c279dc3043186
Luiz <luizhenrique.se@gmail.com>
2017-10-21 00:43:59 -0200

    Adiciona testes de todas as classes

---
+++ src/test/java/com/company/desafioluiz/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.company.desafioluiz.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+}
